75a5da60d74db4a43e1f767a9848d6daf1e40d8b
Sascha Volkenandt <sascha@akv-soft.de>
2017-04-20 23:45:02 +0200

    Implemented library lookup in sketchbook and arduino core libraries folder

--- cmake/Modules/FindArduinoEsp8266Targets.cmake
+++ cmake/Modules/FindArduinoEsp8266Targets.cmake
@@ -1,6 +1,7 @@
 function(add_esp8266_executable name)
     add_executable("${name}" ${ARGN})
-    target_link_libraries("${name}" PUBLIC "${ArduinoEsp8266_LIBRARIES}")
+    target_link_libraries("${name}" PUBLIC ${ArduinoEsp8266_LIBRARIES})
+    target_compile_options("${name}" PUBLIC -include Arduino.h)
 
     add_custom_command(TARGET "${name}" POST_BUILD
             COMMAND "${ESP8266_ESPTOOL}" -eo "${ARDUINO_ESP8266_DIR}/bootloaders/eboot/eboot.elf" -bo "$<TARGET_FILE:${name}>.bin" -bf 40 -bz ${ESP8266_FLASH_SIZE} -bs .text -bp 4096 -ec -eo "$<TARGET_FILE:${name}>" -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec)
