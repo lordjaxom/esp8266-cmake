d3d3c1a5d1ddd98224fd1a40647eb2ebac32ea56
Sascha Volkenandt <sascha@akv-soft.de>
2017-04-23 03:08:04 +0200

    added displaying size of produced binary

--- cmake/Modules/FindArduinoEsp8266Targets.cmake
+++ cmake/Modules/FindArduinoEsp8266Targets.cmake
@@ -3,6 +3,8 @@ function(add_esp8266_executable name)
     target_link_libraries("${name}" PUBLIC ${ArduinoEsp8266_LIBRARIES})
 
     add_custom_command(TARGET "${name}" POST_BUILD
+            COMMAND "${ESP8266_XTENSA_SIZE}" "$<TARGET_FILE:${name}>")
+    add_custom_command(TARGET "${name}" POST_BUILD
             COMMAND "${ESP8266_ESPTOOL}" -eo "${ARDUINO_ESP8266_DIR}/bootloaders/eboot/eboot.elf" -bo "$<TARGET_FILE:${name}>.bin" -bf 40 -bz ${ESP8266_FLASH_SIZE} -bs .text -bp 4096 -ec -eo "$<TARGET_FILE:${name}>" -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec)
 
     set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${name}.bin")
